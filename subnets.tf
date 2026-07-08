8867289afc0f9f99d09b0a444c1426e33ef4e9c7
Gabriel <gabrltakahashi@gmail.com>
2026-07-08 15:14:01 -0300

    feat: add private and public subnets

---
+++ subnets.tf
@@ -0,0 +1,30 @@
+resource "aws_subnet" "public" {
+  count = length(var.availability_zones)
+
+  vpc_id                  = aws_vpc.eks.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = var.availability_zones[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name                                        = "${var.cluster_name}-${var.environment}-public-${var.availability_zones[count.index]}"
+    Environment                                 = var.environment
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                    = "1"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count = length(var.availability_zones)
+
+  vpc_id            = aws_vpc.eks.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name                                        = "${var.cluster_name}-${var.environment}-private-${var.availability_zones[count.index]}"
+    Environment                                 = var.environment
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"           = "1"
+  }
+}
