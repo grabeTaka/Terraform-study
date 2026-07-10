172543a9ef11eedd8edc37bd5290225811d297c8
Gabriel <gabrltakahashi@gmail.com>
2026-07-10 18:20:24 -0300

    feat: add new module cluster

--- main.tf
+++ main.tf
@@ -7,3 +7,12 @@ module "network" {
   public_subnet_cidrs  = var.public_subnet_cidrs
   private_subnet_cidrs = var.private_subnet_cidrs
 }
+
+module "cluster" {
+  source = "./modules/cluster"
+
+  cluster_name = var.cluster_name
+  environment  = var.environment
+  vpc_id       = module.network.vpc_id
+  subnet_ids   = module.network.public_subnet_ids
+}
